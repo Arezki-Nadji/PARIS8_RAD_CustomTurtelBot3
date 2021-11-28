e5ee7747a5460b76c628e2e3bba962e8e17ac78e
AREZKI <arezki.m.nadji96@gmail.com>
2021-11-28 15:32:24 +0100

    Obstacle_Avoidance

---
+++ src/turtelbot_custom/scripts/move_and_avoid.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python3
+
+import rospy
+from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import Twist
+
+disToObstacle =3
+
+def callback(msg):
+	#print to the terminal
+	rospy.loginfo(rospy.get_caller_id() +"The distance to obstacle is - %s", msg.ranges[300])
+#if the distance to an obstacle in front ot the robot is bigger then 1 meter, the robot will move forward
+	if msg.ranges[300]> disToObstacle:
+		print("in")
+		move.linear.x=0.5
+		move.angular.z = 0.0
+
+#if the distance to an obstacle in front of the robot is smaller than 1 metter , the robot will turn left
+	if msg.ranges[300] <= disToObstacle:
+		print("out")
+		move.linear.x =0.
+		move.angular.z = 0.5
+	pub.publish(move)
+
+rospy.init_node('sub_node')
+#we subscribe the laser to the topic
+sub =rospy.Subscriber('/scan',LaserScan,callback)
+pub = rospy.Publisher('/cmd_vel',Twist,queue_size=2)
+rate=rospy.Rate(2)
+move=Twist()
+
+rospy.spin()
